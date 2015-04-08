0245323f034b1205cda6aae3ca1d359e152bc5c9
Ron Gebauer <mail@ron.gebauers.org>
2015-04-08 11:11:54 +0200

    codingstyle

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
+$(window).load(
+		function()
+		{
+			document.addEventListener('tizenhwkey', function(e)
+			{
+				if (e.keyName == "back")
+				{
+					tizen.application.getCurrentApplication().exit();
+				}
+			});
 
-$(window).load(function(){
-	document.addEventListener('tizenhwkey', function(e) {
-        if(e.keyName == "back")
-            tizen.application.getCurrentApplication().exit();
-    });
-	
-	
-	$('.contents').on("click", function(){
-		$('#textbox').html($('#textbox').html() == "Basic" ? "Sample" : "Basic");				
-	});
-	
-	// testing commit
-});
+			$('.contents').on(
+					"click",
+					function()
+					{
+						$('#textbox').html(
+								$('#textbox').html() == "Basic" ? "Sample"
+										: "Basic");
+					});
+		});
